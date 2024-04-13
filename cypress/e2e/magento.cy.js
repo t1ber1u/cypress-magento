9425afb71f110fb0e4bc6a4c5430d804d8bad467
Tibi <tiberiu.chiriac@gmail.com>
2024-04-13 16:44:44 +0300

    added a new spec file and a few changes

--- cypress/e2e/magento.cy.js
+++ cypress/e2e/magento.cy.js
@@ -5,95 +5,39 @@ import Product from '../helpers/product'
 
 const magento = new Magento();
 
-describe('Magento Sample Test', () => {
-    before(() => {
-        magento.visitHomePage();
-        magento.login();
-        magento.emptyCart();
+describe('Order Placement Process:', () => {
+    describe('Adding items with options like size and color to cart', () => {
+        before(() => {
+            cy.clearCookies();
+            cy.clearLocalStorage();
 
-    });
+            //Navigate to the home page
+            magento.visitHomePage();
+            magento.login();
+            magento.emptyCart();
 
-    beforeEach(() => {
-        cy.clearCookies();
-        cy.clearLocalStorage();
+        });
 
-    });
-    it('successfully loads Magento home page and interacts with a product', () => {
-        //Aero Daily Fitness Tee
-        //Wayfarer Messenger Bag
+        beforeEach(() => {
 
-        magento.searchForProductAndGetDetails('Aero Daily Fitness Tee').then(product => {
-            cy.log(`Price: ${product.price}`);
-            cy.log(`Item Link: ${product.itemLink}`);
-            // Adding the product to cart
-            cy.wrap(product).then(p => {
-                p.addToCart(); // Make sure this method is designed to work within Cypress's async nature
-            });
-            // Use the exact selectors to wait for the cart update
-            cy.get('a.action.showcart').within(() => {
-                cy.get('.counter-number').should('have.text', '1'); // Check that the counter number is exactly '1'
-                cy.get('.counter-label span').contains('items'); // Optionally check for the text 'items' if needed
+        });
+        it('should search and add an item to the cart successfully when the options are selected [Add to Cart] button is clicked', () => {
+    
+            magento.searchForProductAndGetDetails('Aero Daily Fitness Tee').then(product => {
+                cy.log(`Price: ${product.price}`);
+                cy.log(`Item Link: ${product.itemLink}`);
+                cy.log(`Item Name: ${product.name}`);
+                // Adding the product to cart
+                cy.wrap(product).then(p => {
+                    p.addToCart({ size: 'M', color: 'Black' }); // Make sure this method is designed to work within Cypress's async nature
+                });
+                // Verify the item is added to the cart
+                cy.get('a.action.showcart').within(() => {
+                    cy.get('.counter-number').should('have.text', '1'); // Check that the counter number is exactly '1'
+                    cy.get('.counter-label span').contains('items'); // Optionally check for the text 'items' if needed
+                });
             });
         });
     });
 
 });
-
-// it('successfully loads Magento home page and interacts with a product', () => {
-//     magento.searchForProductAndGetDetails('Push It Messenger Bag').then(product => {
-//         // Assuming 'product' has been properly instantiated and includes the properties/methods as expected
-//         cy.log(`Price: ${product.price}`); // Properly log within Cypress's chain
-//         cy.log(`Item Link: ${product.itemLink}`); // Log additional information
-
-//         // If you want to perform actions like addToCart, ensure they are implemented to handle within Cypress commands
-//         // For example:
-//         // cy.wrap(product).then(p => p.addToCart()); // If necessary to wrap to handle as a Cypress subject
-//     });
-// });
-
-// // Scenario: Logging in before adding items to cart
-// // Given I am on the login page
-// // When I enter the username and password for the predefined account
-// // And I click on the "Login" button
-// // Then I should be logged in successfully
-
-// Feature: Order Placement
-// Scenario: Adding items to cart
-// Given I am on the home page
-// When I navigate to a product category
-// And I select a product
-// And I click on the "Add to Cart" button
-// Then the item should be added to my cart
-
-// Scenario: Viewing cart
-// Given I have added items to my cart
-// When I click on the "My Cart" link
-// Then I should be taken to the cart page
-// And I should see a list of items in my cart
-
-// Scenario: Updating item quantity in cart
-// Given I am on the cart page
-// When I update the quantity of an item
-// And I click the "Update" button
-// Then the quantity of the item should be updated
-// And the total price should reflect the changes
-
-// Scenario: Removing item from cart
-// Given I am on the cart page
-// When I click the "Remove" link for an item
-// Then the item should be removed from my cart
-// And the total price should be updated accordingly
-
-// Scenario: Proceeding to checkout
-// Given I am on the cart page with items in my cart
-// When I click on the "Proceed to Checkout" button
-// Then I should be taken to the checkout page
-
-// Scenario: Completing checkout with valid information
-// Given I am on the checkout page
-// When I enter valid billing and shipping information
-// And I select a shipping method
-// And I enter payment information
-// And I click on the "Place Order" button
-// Then my order should be placed successfully
-// And I should see a confirmation page with my order details
--- cypress/helpers/product.js
+++ cypress/helpers/product.js
@@ -2,40 +2,61 @@ export default class Product {
     constructor(element) {
         this.element = element;
         this.photo = this.element.find('.product-image-photo').attr('src');
-        this.itemLink = this.element.find('.product-item-link').attr('href');
-        this.name = this.element.find('.product-item-link').text().trim(); 
+        this.itemLink = this.element.find('.product-item-link'); // Capture the jQuery object of the link
+        this.name = this.itemLink.text().trim();  // Extract text directly
         this.reviews = this.element.find('.reviews-actions').text().trim();
         this.price = this.element.find('.price').text().trim();
-        this.addToCart = () => {
-            // Wrap the jQuery element with cy.wrap to use Cypress commands
-            cy.wrap(this.element)
-                .trigger('mouseover')
-                .wait(500) // Wait 500ms for UI to stabilize
-                .find('form[data-role="tocart-form"]')
-                .should('be.visible') // Ensure the form is visible
-                .submit()
-                .then(() => {
-                    // Use the dynamically obtained product name in the success message check
-                    cy.get('.messages .message-success')
-                        .should('contain', `You added ${this.name} to your shopping cart`)
-                        .and('be.visible');
-                });
-        };
-        this.addToWishList = () => {
-            cy.wrap(this.element)
-                .trigger('mouseover')
-                .wait(500) // Wait for any UI transitions
-                .find('.action.towishlist')
-                .should('be.visible') // Check visibility before clicking
-                .click();
-        };
-        this.addToCompare = () => {
-            cy.wrap(this.element)
-                .trigger('mouseover')
-                .wait(500) // Allow time for any animations
-                .find('.action.tocompare')
-                .should('be.visible') // Ensure it is visible before interaction
-                .click();
-        };
+    }
+
+    selectOption(optionType, optionValue) {
+        cy.wrap(this.element)
+            .find(`.swatch-attribute.${optionType} .swatch-option[aria-label="${optionValue}"]`)
+            .click();
+    }
+
+    addToCart(options = {}) {
+        const { size, color } = options;
+        cy.wrap(this.element)
+            .trigger('mouseover')
+            .then(() => {
+                if (size) {
+                    this.selectOption('size', size);
+                }
+                if (color) {
+                    this.selectOption('color', color);
+                }
+            })
+            .then(() => {
+                this.itemLink.trigger('mouseover');
+            })
+            .then(() => {
+                // Wrap the jQuery object into a Cypress object before performing assertions
+                cy.wrap(this.element.find('form[data-role="tocart-form"]'))
+                    .should('be.visible')
+                    .submit()
+                    .then(() => {
+                        cy.get('.messages .message-success')
+                            .should('contain', `You added ${this.name} to your shopping cart`)
+                            .and('be.visible');
+                    });
+            });
+    }
+    
+    addToWishList() {
+        cy.wrap(this.element)
+            .trigger('mouseover')
+            .wait(500) // Wait for any UI transitions
+            .find('.action.towishlist')
+            .should('be.visible') // Check visibility before clicking
+            .click();
+    }
+
+    addToCompare() {
+        cy.wrap(this.element)
+            .trigger('mouseover')
+            .wait(500) // Allow time for any animations
+            .find('.action.tocompare')
+            .should('be.visible') // Ensure it is visible before interaction
+            .click();
     }
 }
